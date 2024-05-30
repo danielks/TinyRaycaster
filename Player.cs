1238f7060a1992975ea0ab349c4502528929c2bb
Daniel K. Silva <danielks@gmail.com>
2024-05-30 09:56:26 -0300

    Refactoring: Main file.

--- Player.cs
+++ Player.cs
@@ -10,7 +10,15 @@ namespace TinyRaycaster
     {
         public float X { get; set; }
         public float Y { get; set; }
-        public float A { get; set; } //view direction
+        public float A { get; set; } //angle the player is facing (the angle between the view direction and the x axis).
         public float FOV { get; set; } //field of view
+
+        public Player(float x, float y, float a, float fOV)
+        {
+            X = x;
+            Y = y;
+            A = a;
+            FOV = fOV;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,16 +11,12 @@ using static System.Net.Mime.MediaTypeNames;
 
 
 
-const int RENDER_WIDTH = 3000;
-const int RENDER_HEIGHT = 1500;
+const int RENDER_WIDTH = 1024;
+const int RENDER_HEIGHT = 512;
 const int WINDOW_WIDTH = RENDER_WIDTH;
 const int WINDOW_HEIGHT = RENDER_HEIGHT;
 
-Player player = new Player();
-player.X = 3.456f;
-player.Y = 2.345f;
-player.A = 1.523f; //angle the player is facing (the angle between the view direction and the x axis).
-player.FOV = MathF.PI / 3.0f;
+Player player = new Player(3.456f, 2.345f, 1.523f, MathF.PI / 3.0f);
 
 string wallTexturesFilePath = @"C:\Users\danie\source\repos\TinyRaycaster\resources\textures.bmp";
 
@@ -104,61 +100,45 @@ void render()
         //makes a line in the direction the player is facing. stops if hits an obstacle.
         for (float t = 0.0f; t < 20.0f; t += 0.05f)
         {
-            float cx = player.X + t * MathF.Cos(angle);
-            float cy = player.Y + t * MathF.Sin(angle);
+            float x = player.X + t * MathF.Cos(angle);
+            float y = player.Y + t * MathF.Sin(angle);
 
             //if (map[float_to_int(cx) + float_to_int(cy) * map_w] != ' ') break;
 
-            int pix_x = Convert.ToInt32(cx * rect_w);
-            int pix_y = Convert.ToInt32(cy * rect_h);
+            int pix_x = Convert.ToInt32(x * rect_w);
+            int pix_y = Convert.ToInt32(y * rect_h);
 
 
             //this draws the visibility cone
             frameBuffer.SetPixel(pix_x, pix_y, new Raylib_cs.Color(160, 160, 160, 255));
 
+
+
             //our ray touches a wall, so draw the vertical column to create an illusion of 3D.
-            if (!map.IsEmpty(Util.float_to_int(cx), Util.float_to_int(cy)))
+            if (map.IsEmpty(Util.float_to_int(x), Util.float_to_int(y))) continue;
+            
+            int cell = map.Get(Util.float_to_int(x), Util.float_to_int(y));
+            int texid = cell;
+
+            int column_height = Util.float_to_int(RENDER_HEIGHT / (t * MathF.Cos(angle - player.A)));
+
+            int x_texcoord = wall_x_texcoord(x, y);
+
+            var column = wallTextures.GetScaledColumn(texid, x_texcoord, column_height);
+
+            pix_x = RENDER_WIDTH / 2 + i;
+
+            for (int j = 0; j < column_height; j++)
             {
-                int cell = map.Get(Util.float_to_int(cx), Util.float_to_int(cy));
-                int texid = cell;
-
-                int column_height = Util.float_to_int(RENDER_HEIGHT / (t * MathF.Cos(angle - player.A)));                
-
-                float hitx = cx - MathF.Floor(cx + 0.5f); // hitx and hity contain (signed) fractional parts of cx and cy,
-                float hity = cy - MathF.Floor(cy + 0.5f); // they vary between -0.5 and +0.5, and one of them is supposed to be very close to 0
-                int x_texcoord = Util.float_to_int(hitx * wallTextures.Size);
-
-                if (MathF.Abs(hity) > MathF.Abs(hitx)) // we need to determine whether we hit a "vertical" or a "horizontal" wall (w.r.t the map)
-                {
-                    x_texcoord = Util.float_to_int(hity * wallTextures.Size);
-                }
-
-                if (x_texcoord < 0) x_texcoord += wallTextures.Size; //do not forget x_texcoord can be negative, fix that
-
-                var column = wallTextures.GetScaledColumn(texid, x_texcoord, column_height);
-
-                pix_x = RENDER_WIDTH / 2 + i;
-
-                for (int j = 0; j < column_height; j++)
-                {
-                    pix_y = j + RENDER_HEIGHT / 2 - column_height / 2;
-                    if (pix_y < 0 || pix_y >= RENDER_HEIGHT) continue;
-                    frameBuffer.SetPixel(pix_x, pix_y, column[j]);
-                }
-
-
-                //divide por 2 pois comeca a desenhar somente na metade da tela. a primeira metade é o mapa.
-                /*draw_rectangle(
-                    WINDOW_WIDTH / 2 + i,
-                    WINDOW_HEIGHT / 2 - column_height / 2,
-                    1,
-                    column_height,
-                    color);*/
-
-                break;
+                pix_y = j + RENDER_HEIGHT / 2 - column_height / 2;
+                if (pix_y < 0 || pix_y >= RENDER_HEIGHT) continue;
+                frameBuffer.SetPixel(pix_x, pix_y, column[j]);
             }
-        }
-    }
+
+            break;
+            
+        } //ray marching loop
+    } //field of view ray sweeping
     //});
 
     Raylib_cs.Image i2 = new Raylib_cs.Image
@@ -179,4 +159,20 @@ void render()
             Raylib.DrawTexture(t2, 0, 0, Raylib_cs.Color.White);            
         }
     }
+}
+
+int wall_x_texcoord(float x, float y)
+{
+    float hitx = x - MathF.Floor(x + 0.5f); // hitx and hity contain (signed) fractional parts of cx and cy,
+    float hity = y - MathF.Floor(y + 0.5f); // they vary between -0.5 and +0.5, and one of them is supposed to be very close to 0
+    int x_texcoord = Util.float_to_int(hitx * wallTextures.Size);
+
+    if (MathF.Abs(hity) > MathF.Abs(hitx)) // we need to determine whether we hit a "vertical" or a "horizontal" wall (w.r.t the map)
+    {
+        x_texcoord = Util.float_to_int(hity * wallTextures.Size);
+    }
+
+    if (x_texcoord < 0) x_texcoord += wallTextures.Size; //do not forget x_texcoord can be negative, fix that
+
+    return x_texcoord;
 }
